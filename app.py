4e7226aa1d4b116252fbf77f2219015313c50878
cjg9480 <47163813+cjg9480@users.noreply.github.com>
2023-04-21 21:45:30 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-view = [100,150,30]
-st.write('# 유튜브 조회수')
-st.write('## 데이타')
+view = [10, 34, 50, 62, 1]
+st.write('# 조사자료 그림')
+st.write('## 자료')
 view
-st.write('## 데이타시트')
+st.write('## 각각 자료값')
 st.bar_chart(view)
 import pandas as pd
 sview = pd.Series(view)
